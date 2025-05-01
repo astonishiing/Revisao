fbc767a3c09126eee47f627e20b0b13a547527c5
astonishiing <wcardoso4510@gmail.com>
2025-05-01 15:13:21 -0300

    Criacao do metodo imprime na classe Autor

--- src/Ex03/domain/Livro.java
+++ src/Ex03/domain/Livro.java
@@ -1,5 +1,14 @@
 package Ex03.domain;
 
 public class Livro {
+    private String nomeLivro;
 
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
 }
