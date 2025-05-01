d33479d938a89e96531e3d2ae286a2b3074a924a
astonishiing <wcardoso4510@gmail.com>
2025-05-01 15:34:37 -0300

    Associação bidirecional

--- src/Ex03/domain/Livro.java
+++ src/Ex03/domain/Livro.java
@@ -2,7 +2,31 @@ package Ex03.domain;
 
 public class Livro {
     private String nomeLivro;
+    private Autor[] autores;
 
+    public Livro(){
+
+    }
+    public Livro(String nomeLivro){
+        this.nomeLivro = nomeLivro;
+    }
+
+    public void imprimeLivrosAutores(){
+        System.out.println(this.nomeLivro);
+        if(autores != null){
+            for (Autor autor : autores) {
+                System.out.println(autor.getNomeCompleto());
+            }
+        }
+    }
+
+    public Autor[] getAutores() {
+        return autores;
+    }
+
+    public void setAutores(Autor[] autores) {
+        this.autores = autores;
+    }
 
     public String getNomeLivro() {
         return nomeLivro;
