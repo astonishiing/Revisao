d8f1903ab50ff46996f9c33cf732dd2a72a81ca1
astonishiing <wcardoso4510@gmail.com>
2025-05-01 15:08:26 -0300

    Criacao dos contrutores de Aturo

--- src/Ex03/domain/Autor.java
+++ src/Ex03/domain/Autor.java
@@ -1,8 +1,20 @@
 package Ex03.domain;
 
 public class Autor {
-
+    private String nomeCompleto;
+    private String biografiaAutor;
+    private int anoNascimento;
     private Livro objLivro;
+
+    public Autor(){
+
+    }
+    public Autor(String nomeCompleto, String biografiaAutor, int anoNascimento){
+        this.nomeCompleto = nomeCompleto;
+        this.biografiaAutor = biografiaAutor;
+        this.anoNascimento = anoNascimento;
+    }
+
 
 
 }
