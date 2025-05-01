5917c96fa9e8954a50da5e390b5820091d1e0148
astonishiing <wcardoso4510@gmail.com>
2025-05-01 14:41:21 -0300

    Criacao do ex02

--- src/Ex02/test/Ex02Test.java
+++ src/Ex02/test/Ex02Test.java
@@ -1,4 +1,17 @@
 package Ex02.test;
 
+import Ex02.domain.Ex02;
+
+import java.util.Scanner;
+
 public class Ex02Test {
+    public static void main(String[] args) {
+        Ex02 ex02 = new Ex02();
+        Ex02.setNumero1(5);
+        Ex02.setNumero2(2);
+
+        ex02.imprime();
+    }
+
+
 }
